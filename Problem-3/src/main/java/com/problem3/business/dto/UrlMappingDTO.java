da9bdc38cb57ef59e9f7d99885dbe3a3f8b6a597
kishor.dige <kishordige@syspiretechnologies.com>
2021-07-04 16:18:20 +0530

    added lombok dependency

--- Problem-3/src/main/java/com/problem3/business/dto/UrlMappingDTO.java
+++ Problem-3/src/main/java/com/problem3/business/dto/UrlMappingDTO.java
@@ -1,41 +1,10 @@
 package com.problem3.business.dto;
 
+import lombok.Data;
+
+@Data
 public class UrlMappingDTO {
 	private String shortUrl;
 	private String url;
 	
-	public UrlMappingDTO() {
-		super();
-	}
-
-	public UrlMappingDTO(String shortUrl, String url) {
-		super();
-		this.shortUrl = shortUrl;
-		this.url = url;
-	}
-
-	@Override
-	public String toString() {
-		return "UrlMappingDTO [shortUrl=" + shortUrl + ", url=" + url + "]";
-	}
-
-	public String getShortUrl() {
-		return shortUrl;
-	}
-
-	public void setShortUrl(String shortUrl) {
-		this.shortUrl = shortUrl;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	
-	
-
 }
--- Problem-3/src/main/java/com/problem3/integration/domain/UrlMapping.java
+++ Problem-3/src/main/java/com/problem3/integration/domain/UrlMapping.java
@@ -5,69 +5,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class UrlMapping {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long mappingId;
-
 	private String shortUrl;
 	private String url;
 	private String uniqueId;
-
-	public UrlMapping() {
-		super();
-	}
-
-	public UrlMapping(Long mappingId, String shortUrl, String url, String uniqueId) {
-		super();
-		this.mappingId = mappingId;
-		this.shortUrl = shortUrl;
-		this.url = url;
-		this.uniqueId = uniqueId;
-	}
-
-	@Override
-	public String toString() {
-		return "UrlMapping [mappingId=" + mappingId + ", shortUrl=" + shortUrl + ", url=" + url + ", uniqueId="
-				+ uniqueId + "]";
-	}
-
-	public Long getMappingId() {
-		return mappingId;
-	}
-
-	public void setMappingId(Long mappingId) {
-		this.mappingId = mappingId;
-	}
-
-	public String getShortUrl() {
-		return shortUrl;
-	}
-
-	public void setShortUrl(String shortUrl) {
-		this.shortUrl = shortUrl;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public String getUniqueId() {
-		return uniqueId;
-	}
-
-	public void setUniqueId(String uniqueId) {
-		this.uniqueId = uniqueId;
-	}
-
 	
-
-	
-
 }
