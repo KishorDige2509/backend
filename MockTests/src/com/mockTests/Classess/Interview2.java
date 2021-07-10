336c875edae2d39d490336bd21d2024f71d12deb
kishor.dige <kishordige@syspiretechnologies.com>
2021-07-10 11:46:17 +0530

    interview 2

--- MockTests/src/com/mockTests/Classess/Interview2.java
+++ MockTests/src/com/mockTests/Classess/Interview2.java
@@ -2,25 +2,26 @@ package com.mockTests.Classess;
 
 public class Interview2 {
 	public static void main(String[] args) {
-	
-			int[] arr = {0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1};
-			int length = arr.length;
-			for(int i=0; i<arr.length; i++){
-				
-				if(arr[i] == 0 && arr[length-(i+1)]==1){
-					
-					continue;
-				} else {
-					arr[i]=0;
-					arr[length-(i+1)]=1;
-				}
-			}
 
-			for(int i=0; i<arr.length; i++){
-				System.out.print(arr[i] + " ");
+	}
+
+	static void arrange() {
+		int[] arr = { 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1 };
+		int length = arr.length;
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] == 0 && arr[length - (i + 1)] == 1) {
+
+				continue;
+			} else {
+				arr[i] = 0;
+				arr[length - (i + 1)] = 1;
 			}
-			
-		
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 
 }
