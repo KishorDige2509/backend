1a23a614149ac742df21e3f36e79cf96bf557531
kishor.dige <kishor.dige@syspiretechnologies.com>
2022-07-31 01:34:27 +0530

    added more patterns

--- JavaNewApiFeatures/src/com/patterns/Triangle.java
+++ JavaNewApiFeatures/src/com/patterns/Triangle.java
@@ -5,6 +5,23 @@ public class Triangle {
 		solidRightAngledTriangle();
 		System.out.println();
 		hollowRightAngledTriangle();
+		System.out.println();
+		reverseRightAngledTriangle();
+		System.out.println();
+		reverseHollowRightAngledTriangle();
+	}
+
+	private static void reverseHollowRightAngledTriangle() {
+		for(int i=1; i<=5; i++) {
+			for(int j=5; j>=1; j--) {
+				if(j==5 || i==1 || i==j) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 	private static void hollowRightAngledTriangle() {
@@ -25,6 +42,17 @@ public class Triangle {
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= 5; j++) {
 				if (j <= i) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	public static void reverseRightAngledTriangle() {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				if(i<=j) {
 					System.out.print("*");
 				}
 			}
