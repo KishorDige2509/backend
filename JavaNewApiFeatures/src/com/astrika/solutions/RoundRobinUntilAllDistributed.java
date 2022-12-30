36b62171b38f690b51adeae0e43e1a93b061f9c1
KishorDige2509 <kishordige26@gmail.com>
2022-12-31 02:25:30 +0530

    added check for overall buckets

--- JavaNewApiFeatures/src/com/astrika/solutions/RoundRobinUntilAllDistributed.java
+++ JavaNewApiFeatures/src/com/astrika/solutions/RoundRobinUntilAllDistributed.java
@@ -1,14 +1,15 @@
 package com.astrika.solutions;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class RoundRobinUntilAllDistributed {
 
 	public static void main(String[] args) {
 		// Create a list of elements to be distributed
-		List<Integer> elements = new ArrayList<>();
-		for (int i = 1; i <= 10; i++) {
+		List<Integer> elements = new LinkedList<>();
+		for (int i = 1; i <= 2000; i++) {
 			elements.add(i);
 		}
 
@@ -21,7 +22,7 @@ public class RoundRobinUntilAllDistributed {
 		people.add(new Person("Dave", 5));
 		
 		// fetch last Index of distribution for this set of people
-		int lastDistributionIndex = 1;
+		int lastDistributionIndex = -1;
 
 
 		// Distribute the elements to the people in a round-robin fashion, distributing
@@ -46,21 +47,40 @@ public class RoundRobinUntilAllDistributed {
 	}
 
 	public static <T> List<List<T>> distributeElements(List<T> elements, List<Person> people, int lastDistributionIndex) {
+				
 		// Create a list of lists to hold the distributions for each person
 		List<List<T>> distributions = new ArrayList<>();
 		for (int i = 0; i < people.size(); i++) {
 			distributions.add(new ArrayList<T>());
 		}
+		
+		System.out.println("People size: " + people.size());
+		System.out.println("last distribution index: " + lastDistributionIndex);
 
 		// Distribute the elements in a round-robin fashion, until the capacity of each
 		// person is reached
-		int personIndex = lastDistributionIndex;
-		while (!elements.isEmpty()) {
+		int personIndex;
+		// check last distribution index and set max value if greater than array size and zero in case of negative
+		if(lastDistributionIndex>=people.size()) {
+			personIndex = people.size()-1;
+		} else if(lastDistributionIndex<0) {
+			personIndex = 0;
+		} else {
+			personIndex = lastDistributionIndex;
+		}
+		System.out.println("Start index of person: " + personIndex);
+		int threshold = 0;
+		int overallCapacity = people.size();
+		while (!elements.isEmpty() && threshold<overallCapacity) {
 			Person person = people.get(personIndex);
+			System.out.println("Adding leads for : " + person.name + " with container Index:" + personIndex);
 			if (person.capacity > distributions.get(personIndex).size()) {
 				distributions.get(personIndex).add(elements.remove(0));
-			} 
-			personIndex = (personIndex + 1) % people.size();			
+			} else {
+				threshold++;
+				System.out.println("Capacity Reached for Person: " + person.name);
+			}
+			personIndex = (personIndex + 1) % people.size();	
 		}
 
 		// Distribute the remaining elements in a round-robin fashion over the capacity
