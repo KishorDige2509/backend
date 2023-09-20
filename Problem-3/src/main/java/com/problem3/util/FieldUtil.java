7e956cfc1fb751a805d77658f489355955ca6802
kishor.dige <kishor.dige@syspiretechnologies.com>
2023-09-20 17:57:18 +0530

    removed unused method

--- Problem-3/src/main/java/com/problem3/util/FieldUtil.java
+++ Problem-3/src/main/java/com/problem3/util/FieldUtil.java
@@ -21,6 +21,14 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class FieldUtil {
 
+	private static final String UPDATED_EMPTY_MANDATOY_FIELDS_SET = "Updated emptyMandatoyFieldsSet:{}";
+
+	private static final String ADDING_FIELDS_TO_EMPTY_MANDATOY_FIELDS_SET = "Adding Fields to emptyMandatoyFieldsSet ...";
+
+	private static final String FIELD_VALUE_FOUND_NULL_OR_EMPTY = "Field Value found null or empty:{}";
+
+	private static final String FIELD_NAME_FOUND_NULL_OR_EMPTY = "Field Name found null or empty:{}";
+
 	private static final String CLASSNAME = FieldUtil.class.getSimpleName();
 
 	private static final String FIELD_VALUE_TO_CHECK = "Field value to check:{}";
@@ -124,6 +132,7 @@ public class FieldUtil {
 	private void findEmptyAndNullFieldsInJsonObject(List<String> parentFieldNames, ObjectNode objectNode,
 			Set<String> emptyMandatoryFieldsInDto) {
 		log.info(LogUtil.startLog(CLASSNAME));
+		log.info("Node to check for empty mandatory fields:{}", objectNode);
 		log.info("Empty Mandatory fields in DTO:{}", emptyMandatoryFieldsInDto);
 		Iterator<Map.Entry<String, JsonNode>> fields = objectNode.fields();
 		while (fields.hasNext()) {
@@ -134,17 +143,91 @@ public class FieldUtil {
 			log.info(FIELD_NAME, fieldName);
 			log.info(FIELD_VALUE, fieldValue);
 
-			if (parentFieldNames.contains(fieldName) && fieldIsNullOrEmpty(fieldValue)) {
-				log.info("Field Name found null or empty:{}", fieldName);
-				log.info("Field Value found null or empty:{}", fieldValue);
-				log.info("Adding Fields to emptyMandatoyFieldsSet ...");
-				emptyMandatoryFieldsInDto.add(fieldName);
-				log.info("Updated emptyMandatoyFieldsSet:{}", emptyMandatoryFieldsInDto);
+			if (parentFieldNames.contains(fieldName)) {
+				addFieldToSetIfEmpty(emptyMandatoryFieldsInDto, fieldName, fieldValue);
 			} else {
 				findEmptyAndNullFieldsInJsonNode(parentFieldNames, fieldValue, emptyMandatoryFieldsInDto);
 			}
 		}
 		log.info(LogUtil.exitLog(CLASSNAME));
+	}
+
+	private void addFieldToSetIfEmpty(Set<String> emptyMandatoryFieldsInDto, String fieldName, JsonNode fieldValue) {
+		log.info(LogUtil.startLog(CLASSNAME));
+		if (fieldIsInstanceOfObject(fieldValue)) {
+			if (isAnyFieldEmptyInObject((ObjectNode) fieldValue)) {
+				addFieldsInSet(emptyMandatoryFieldsInDto, fieldName, fieldValue);
+			}
+		} else if (fieldIsInstanceOfArray(fieldValue)) {
+			if (isAnyElementEmptyInArray((ArrayNode) fieldValue)) {
+				addFieldsInSet(emptyMandatoryFieldsInDto, fieldName, fieldValue);
+			}
+		} else if (isFieldNullOrEmpty(fieldValue)) {
+			addFieldsInSet(emptyMandatoryFieldsInDto, fieldName, fieldValue);
+		}
+		log.info(LogUtil.exitLog(CLASSNAME));
+	}
+
+	private boolean isFieldNullOrEmpty(JsonNode fieldValue) {
+		log.info(LogUtil.startLog(CLASSNAME));
+		log.info(FIELD_VALUE_TO_CHECK, fieldValue);
+		boolean test = fieldValue == null || fieldValue.isNull() || fieldValue.asText().isEmpty();
+		log.info("Is Field Null or Empty:{}", test);
+		log.info(LogUtil.exitLog(CLASSNAME));
+		return test;
+	}
+
+	private void addFieldsInSet(Set<String> emptyMandatoryFieldsInDto, String fieldName, JsonNode fieldValue) {
+		log.info(LogUtil.startLog(CLASSNAME));
+		log.info(FIELD_NAME_FOUND_NULL_OR_EMPTY, fieldName);
+		log.info(FIELD_VALUE_FOUND_NULL_OR_EMPTY, fieldValue);
+		log.info(ADDING_FIELDS_TO_EMPTY_MANDATOY_FIELDS_SET);
+		emptyMandatoryFieldsInDto.add(fieldName);
+		log.info(UPDATED_EMPTY_MANDATOY_FIELDS_SET, emptyMandatoryFieldsInDto);
+		log.info(LogUtil.exitLog(CLASSNAME));
+	}
+
+	private boolean isAnyElementEmptyInArray(ArrayNode fieldValue) {
+		log.info(LogUtil.startLog(CLASSNAME));
+		log.info(FIELD_VALUE_TO_CHECK, fieldValue);
+		boolean test = false;
+		// Check each element in the array
+		for (JsonNode element : fieldValue) {
+			if (element == null || element.isNull() || element.asText().isEmpty()) {
+				test = true;
+				break;
+			}
+		}
+		log.info("Is Field Null or Empty:{}", test);
+		log.info(LogUtil.exitLog(CLASSNAME));
+		return test;
+	}
+
+	private boolean isAnyFieldEmptyInObject(ObjectNode objectNode) {
+		log.info(LogUtil.startLog(CLASSNAME));
+		log.info("Node to check for empty mandatory fields: {}", objectNode);
+
+		Iterator<Map.Entry<String, JsonNode>> fields = objectNode.fields();
+		while (fields.hasNext()) {
+			Map.Entry<String, JsonNode> field = fields.next();
+			String fieldName = field.getKey();
+			JsonNode fieldValue = field.getValue();
+
+			log.info(FIELD_NAME, fieldName);
+			log.info(FIELD_VALUE, fieldValue);
+
+			if (fieldIsInstanceOfObject(fieldValue)) {
+				if (isAnyFieldEmptyInObject((ObjectNode) fieldValue)) {
+					log.info(LogUtil.exitLog(CLASSNAME));
+					return true;
+				}
+			} else if (isFieldNullOrEmpty(fieldValue)) {
+				log.info(LogUtil.exitLog(CLASSNAME));
+				return true;
+			}
+		}
+		log.info(LogUtil.exitLog(CLASSNAME));
+		return false;
 	}
 
 	private void findEmptyAndNullFieldsInJsonArray(List<String> parentFieldNames, ArrayNode arrayNode,
@@ -278,28 +361,6 @@ public class FieldUtil {
 			}
 		}
 		log.info(LogUtil.exitLog(CLASSNAME));
-	}
-
-	private boolean fieldIsNullOrEmpty(JsonNode fieldValue) {
-		log.info(LogUtil.startLog(CLASSNAME));
-		log.info(FIELD_VALUE_TO_CHECK, fieldValue);
-		boolean test = false;
-		if (fieldValue.isArray()) {
-			// Check each element in the array
-			for (JsonNode element : fieldValue) {
-				if (element == null || element.isNull() || element.asText().isEmpty()) {
-					test = true;
-					break;
-				}
-			}
-
-		} else {
-			test = fieldValue.isNull() || fieldValue.asText().isEmpty();
-		}
-
-		log.info("Is Field Null or Empty:{}", test);
-		log.info(LogUtil.exitLog(CLASSNAME));
-		return test;
 	}
 
 	private boolean fieldIsInstanceOfObject(JsonNode fieldValue) {
